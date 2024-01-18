32122ec14e02be427b06f29ee3ccab37bdd49364
AlexHVL <673612@stud.hvl.no>
2024-01-18 14:23:32 +0100

    Initial commit

---
+++ src/no/hvl/data102/filmarkiv/impl/Film.java
@@ -0,0 +1,22 @@
+package no.hvl.data102.filmarkiv.impl;
+
+import no.hvl.data102.filmarkiv.impl.Sjanger;
+
+public class Film {
+	
+	private int filmNr;
+	private int lanserigsAar;
+	
+	private String produsent;
+	private String tittel;
+	private String filmSelskap;
+	
+	private Sjanger sjanger;
+	
+	public Film(int filmNr, int lanserigsAar, String produsent, String tittel) {
+		
+	}
+	
+	
+
+}
